56be6ee09b80b68da30cb8e134f7c59021f9b1f0
Chinedu <neduvinci@gmail.com>
2019-08-21 14:51:44 +0100

    continued working on this.

--- assignments/this.js
+++ assignments/this.js
@@ -1,8 +1,9 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. When there is no object defined or 
-* 2. 
+* 1. When this is mentioned on a global scope, it has window/global object binding since the window is an object.  
+     This means that there is no other object defined or 'this' is not mmentioned in an object literal, object construct or construct state.
+* 2. When a function mentioned after a dot, the this refers to the object just before the dot.
 * 3. 
 * 4. 
 *
@@ -12,10 +13,19 @@
 // Principle 1
 
 // code example for Window Binding
+// console.log(this);
 
 // Principle 2
 
 // code example for Implicit Binding
+let fuzzyBrain = {
+    name: 'Lambda student on day 3 of JS',
+    greet: function(){
+        return `Hi I'm a ${this.name} and I can't take this anymore`; 
+    },
+}
+
+fuzzyBrain.greet();
 
 // Principle 3
 
